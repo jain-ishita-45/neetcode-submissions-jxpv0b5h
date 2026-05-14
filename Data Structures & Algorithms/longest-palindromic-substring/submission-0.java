c4582f4ca93f95bf4625f0a22075be028e8a8662
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-05-14 23:00:44 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestPalindrome(String s) {
+       int residx=0,reslen=0;
+        int n=s.length();
+        boolean dp[][]=new boolean[n][n];
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i;j<n;j++)
+            {
+                if(s.charAt(i)==s.charAt(j) && (j-i<=2 || dp[i+1][j-1]))
+                {
+                    dp[i][j]=true;
+                    if(reslen<(j-i+1))
+                    {
+                        residx=i;
+                        reslen=j-i+1;
+                    }
+                }
+            }
+           
+        }
+         return s.substring(residx,residx+reslen);
+    }
+}
