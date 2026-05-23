8d66ccf3831a9ee96010f773e5f9a3c1c5d5291f
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-05-23 11:17:27 +0530

    Add: interleaving-string - submission-1

---
+++ Data Structures & Algorithms/interleaving-string/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+      if(s1.length()+s2.length()!=s3.length()) return false;
+      Boolean dp[][]=new Boolean[s1.length()+1][s2.length()+1];
+      return solve(0,0,s1,s2,s3,dp);
+
+    }
+
+    public Boolean solve(int i, int j, String s1,String s2,String s3, Boolean [][]dp)
+    {
+        if(i==s1.length() && j==s2.length()) return true;
+        if(dp[i][j]!=null) return dp[i][j];
+        Boolean res=false;
+        if(i<s1.length() && i+j<s3.length() && s1.charAt(i)==s3.charAt(i+j) )
+        res=solve(i+1,j,s1,s2,s3,dp);
+        if(j<s2.length() && i+j<s3.length() && s2.charAt(j)==s3.charAt(i+j))
+        res =solve(i,j+1,s1,s2,s3,dp);
+        dp[i][j]=res;
+        return res;
+
+    }
+}
