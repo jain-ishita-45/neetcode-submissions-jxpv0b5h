25794ace69b9d1803bda0e849435c16bca6baeec
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-06-02 19:44:38 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+  
+    public double myPow(double x, int n) {
+        if(n<0)
+        {
+            x=1/x;
+            n=(-1)*n;
+        }
+        return power(x,n);
+    }
+
+    public double power(double x, int n)
+    {
+        if(n==0) return 1;
+        double half=power(x,n/2);
+        if(n%2==0)
+        return half*half;
+        else return half*x*half;
+        
+    }
+}
